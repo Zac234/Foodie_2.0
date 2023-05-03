6ed6b02335f7ef850e6091a4c140f33432b0fbb2
Zach <zacbenincasa@gamil.com>
2023-05-02 20:19:07 -0600

    added in the main login page and will add the client and restaurant parts in .

---
+++ src/views/Main_Sign_in.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <img src="" alt="Foodie logo">
+            <h1>Foodie</h1>
+        </header>
+        
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
